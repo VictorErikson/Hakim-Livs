6da992b2a27bfe12d0957e0f36b204eec1169303
Lina Svensson <lina.kuzmanowsky@gmail.com>
2025-03-27 14:26:15 +0100

    functionality

--- src/services/showCategories.js
+++ src/services/showCategories.js
@@ -8,6 +8,16 @@ export  async function showCategories(products) {
     const categoryContainer = document.querySelector('#categoryContainer');
     if (!categoryContainer) return;
 
+    //products is an array of product objects
+    //each product has a key 'kategorier' which is an array of categories
+    //.flatMap() goes through each product and collects all the categories into
+    //one single flat array. EXAMPLE:
+//     [{ kategorier: [a, b] }, { kategorier: [b, c] }]
+// → flatMap → [a, b, b, c]
+
+// A set is a special object that only stores unique values
+// so by wrapping the array from flatMap in new Set() we automatically remove duplicates
+// ... (spread operator) converts the set back into a normal Arr so we can loop over it later. otherwise doesnt work as well with forEach
     const uniqueCategories = [
         ...new Set(products.flatMap(product => product.kategorier)),
     ];
