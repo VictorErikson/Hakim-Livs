66df9593194a7ef9c63f06a80f7b2d6988484e05
Lina Svensson <lina.kuzmanowsky@gmail.com>
2025-04-03 13:21:12 +0200

    colon

--- src/services/admin/deleteItem.js
+++ src/services/admin/deleteItem.js
@@ -3,7 +3,7 @@ import { fetchProducts } from "../../utils/api.js";
 
 export async function deleteItem (id) {
     try{
-        const response = await axios.delete(`https://grupp-11-backend.vercel.app/api/products/${id}`, {
+        const response = await axios.delete(`https://grupp-11-backend.vercel.app/api/products/:${id}`, {
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${sessionStorage.getItem("jwt")}`
