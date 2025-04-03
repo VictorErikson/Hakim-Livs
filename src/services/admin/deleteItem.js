0b5afc61fac9ef077330a7bd41240c9675849cad
Lina Svensson <lina.kuzmanowsky@gmail.com>
2025-04-03 12:56:45 +0200

    hej

--- src/services/admin/deleteItem.js
+++ src/services/admin/deleteItem.js
@@ -1,13 +1,18 @@
-
+import { showProductsAdmin } from "../services/showProductsAdmin.js";
+import { fetchProducts } from "../utils/api.js";
 
 export async function deleteItem (id) {
     try{
         const response = await axios.delete(`https://grupp-11-backend.vercel.app/api/products/${id}`, {
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem("jwt")}`
+                Authorization: `Bearer ${sessionStorage.getItem("jwt")}`
             }
         });
+        if (response.status === 200) {
+            products = await fetchProducts();
+            showProductsAdmin(products);
+        }
     }catch(error){
         console.log("Något gick fel: " + error);
     }
