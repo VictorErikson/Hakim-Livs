3faf36b474e9e2588a25866e4e0f95b2a1cda96f
Lina Svensson <lina.kuzmanowsky@gmail.com>
2025-04-03 16:35:54 +0200

    de

--- src/services/showProductsAdmin.js
+++ src/services/showProductsAdmin.js
@@ -59,11 +59,13 @@ export function showProductsAdmin(products) {
         document.getElementById("ammount").value = product.mängd;
         document.getElementById("brand").value = product.varumärke || product.varumarke || "";
         document.getElementById("content").value = product.innehållsförteckning;
-        document.getElementById("compare").value = parseFloat(
-            product.jämförelsepris.replace(" kr/kg", "").replace(",", ".")
-          );
+        document.getElementById("compare").value = product.jämförelsepris
+  ? parseFloat(product.jämförelsepris.replace(" kr/kg", "").replace(",", "."))
+  : "";
         document.getElementById("supplier").value = product.leverantör;
 
+        console.log("Editing product:", product);
+        
         sessionStorage.setItem('editProductId', product._id);
 
         //update button to show edit mode
